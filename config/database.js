9a081c4bfe89cfdaff3dd33c2a0ff67b40635b9f
Nicole B <63973798+NicoleBazile@users.noreply.github.com>
2020-09-24 09:01:22 -0400

    Delete database.js

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : "mongodb+srv://NBwebdev:8n5LLTKbQGn87b8@cluster0.fe6hb.mongodb.net/megan?retryWrites=true&w=majority", 
-    'dbName': "megan"
-};
